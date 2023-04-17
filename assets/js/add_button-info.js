18b936c796a8f41434ba02a9743c5f54e7597e4b
nattdev <110750471+nattdev@users.noreply.github.com>
2023-04-17 14:52:54 -0500

    Create add_button-info.js

---
+++ assets/js/add_button-info.js
@@ -0,0 +1,6 @@
+if(window.location.href == "https://pag-demo3.blogspot.com/"){
+      let button_info = document.createElement("a");
+      button_info.textContent = "Más información";
+      button_info.href = "p/servicios.html";
+      document.querySelector("#Image14 span.caption").appendChild(button_info);
+}
