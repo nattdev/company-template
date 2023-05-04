858f3ccfcca6d87579ae12400f58371e3e92065b
nattdev <110750471+nattdev@users.noreply.github.com>
2023-05-04 00:16:33 -0500

    Create scroll-behavior.js

---
+++ v3/assets/js/scroll-behavior.js
@@ -0,0 +1,20 @@
+var el_LinkList1 = document.querySelectorAll("div#LinkList1 ul li a"); 
+var el_LinkList2 = [];
+var list_fixed = document.querySelector("#LinkList1");
+
+for(var i = 0; i < document.querySelectorAll("div#LinkList1 ul li a").length - 1; i++){
+el_LinkList2[i] =  "div#LinkList1 ul li:nth-child" + "(" + (i + 2) + ") a";
+}
+                   
+let tarjet = ["#services_section", "#info_section", "#testimonials_section", "#additem_section", "#main", "#form_section"];
+
+for(var i = 0; i < document.querySelectorAll("div#LinkList1 ul li a").length - 1; i++){
+  let offsetTop = document.querySelector(tarjet[i]).offsetTop;
+  $(el_LinkList2[i]).on("click", function(e){
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: offsetTop - menu_top }, 1300);
+  if(window.innerWidth < 1270) {
+    list_fixed.style.display = "none";
+  }
+  });
+}
