5762512727394656ab15f11c7c94bb74a89fa430
nattdev <110750471+nattdev@users.noreply.github.com>
2023-06-03 20:48:02 -0500

    Create addWrappers.js

---
+++ v3/assets/js/addWrappers.js
@@ -0,0 +1,7 @@
+var myElements = document.querySelectorAll("#presentation_section > div");
+var myWrapper = document.createElement("div");
+wrapAll(myElements, myWrapper);
+  
+var myElements2 = document.querySelectorAll("#form_section .widget.Text");
+var myWrapper2 = document.createElement("div");
+wrapAll(myElements2, myWrapper2);
