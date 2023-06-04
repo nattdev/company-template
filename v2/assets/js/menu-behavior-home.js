81b0d283e807fdb6d2ac510211b6f78681cf6dd1
nattdev <110750471+nattdev@users.noreply.github.com>
2023-06-04 15:02:39 -0500

    Update menu-behavior-home.js

--- v2/assets/js/menu-behavior-home.js
+++ v2/assets/js/menu-behavior-home.js
@@ -2,7 +2,7 @@
           document.addEventListener("scroll", function(){
           	var menu_fixed = document.querySelector("#header");
    
-          	if(window.scrollY > menu_top){
+          	if(window.scrollY > menu_top || window.innerWidth <= 1270 ){
           	menu_fixed.style.background = "var(--main-bg-color)";
           	menu_fixed.style.display = "flex";
          	} else {
