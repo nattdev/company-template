06d2c5f093b3a3f77a72d064842c89a02d218952
nattdev <110750471+nattdev@users.noreply.github.com>
2023-10-20 21:23:08 -0500

    Update hidden_menu-links.js

--- v2/assets/js/hidden_menu-links.js
+++ v2/assets/js/hidden_menu-links.js
@@ -1,7 +1,8 @@
 var linklist = document.querySelectorAll("#LinkList1 ul li")
-for(var i = 1; i < linklist.length; i++){
-  linklist[i].style.display = "none";
+for (var i = 1; i < linklist.length; i++) {
+	linklist[i].style.display = "none";
 }
 //Menu styles
 var menu_top = document.querySelector("#header");
-menu_top.style.background = "var(--main-bg-color)";
+menu_top.style.background = "rgb(var(--main-bg-color), 0.50)";
+menu_top.style.alignItems = "center";
