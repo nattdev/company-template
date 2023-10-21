f103bcf5170e088145bce1f11fc05f923a0ad4ad
nattdev <110750471+nattdev@users.noreply.github.com>
2023-10-20 21:21:25 -0500

    Update menu-behavior-home.js

--- v2/assets/js/menu-behavior-home.js
+++ v2/assets/js/menu-behavior-home.js
@@ -1,11 +1,10 @@
- var menu_top = document.querySelector("#header").offsetHeight;
-          document.addEventListener("scroll", function(){
-          	var menu_fixed = document.querySelector("#header");
-   
-          	if(window.scrollY > menu_top || window.innerWidth <= 1270 ){
-          	menu_fixed.style.background = "var(--main-bg-color)";
-          	menu_fixed.style.display = "flex";
-         	} else {
-          	menu_fixed.style.background = "transparent";
-          }
-          });
+var menu_top = document.querySelector("#header").offsetHeight;
+document.addEventListener("scroll", function() {
+	var menu_fixed = document.querySelector("#header");
+	if (window.scrollY > menu_top || window.innerWidth <= 1270) {
+		menu_fixed.style.background = "rgb(var(--main-bg-color), 0.50)";
+		menu_fixed.style.display = "flex";
+	} else {
+		menu_fixed.style.background = "transparent";
+	}
+});
